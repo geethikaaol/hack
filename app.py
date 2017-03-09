d6ea423ef95cde1314c6e2c4fef0d76e11a6cdf0
geethikaaol <geethikakotipalli93@gmail.com>
2017-03-10 00:56:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,10 +35,10 @@ def makeWebhookResult(req):
     date = parameters.get("date")
 
     baseurl = "http://192.168.1.4:8080"
-    
+    result = urlopen(yql_url).read()
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to  is " + str(date) + " euros."
+    speech = "The cost of shipping to  is " + str(json.loads(result)) + " euros."
 
     print("Response:")
     print(speech)
