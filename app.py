8b0231ff3beb1f36239af8d3c4dd8c96e646affb
geethikaaol <geethikakotipalli93@gmail.com>
2017-03-10 01:39:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from urllib2 import urlopen
 from flask import Flask
 from flask import request
 from flask import make_response
-from flask import jsonify
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -38,7 +37,7 @@ def makeWebhookResult(req):
 
     baseurl = "http://192.168.1.4:8080"
     url = "https://jsonplaceholder.typicode.com/posts"
-    response = urlopen(url,jsonify(parameters)).read()
+    response = urlopen(url,json.dumps(parameters)).read()
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
     speech = "The cost of shipping to  is " + str(date) + " euros."
