c13a9e7ea5133cc855bda9d5fd1ecb0c510fcf48
geethikaaol <geethikakotipalli93@gmail.com>
2017-03-10 01:01:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
-import urllib
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.request import urlopen, Request
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
 import os
 
 from flask import Flask
 from flask import request
 from flask import make_response
-from urllib.request import urlopen, Request
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -36,7 +42,7 @@ def makeWebhookResult(req):
     date = parameters.get("date")
 
     baseurl = "http://192.168.1.4:8080"
-    result = urlopen(yql_url).read()
+    #result = urlopen(yql_url).read()
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
     speech = "The cost of shipping to  is " + str(json.loads(result)) + " euros."
